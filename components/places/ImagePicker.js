7657efe84ca0a73e9402e07f0361ce5c078bc92f
Jesus <deonatto@gmail.com>
2023-03-06 23:16:16 +0000

    create button, and show image in screen

--- components/places/ImagePicker.js
+++ components/places/ImagePicker.js
@@ -1,25 +1,53 @@
-import React from "react";
-import { View, Button, Alert } from "react-native";
-import {
-  launchImageLibraryAsync,
-  MediaTypeOptions,
-} from "expo-image-picker";
+import React, { useState } from "react";
+import { View, Button, Image, Text, StyleSheet } from "react-native";
+import { MediaTypeOptions, launchImageLibraryAsync } from "expo-image-picker";
+import { GlobalStyles } from "../../constants/colors";
+import OutlinedButton from "../ui/OutlinedButton";
 
 const ImagePicker = () => {
-  const takeImageHandler = async () => {
-    const image = await launchImageLibraryAsync({
+  const [image, setImage] = useState(null);
+  const pickImageHandler = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await launchImageLibraryAsync({
       mediaTypes: MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [16, 9],
+      aspect: [4, 3],
       quality: 1,
     });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
   };
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imageContainer}>
+        {image ? (
+          <Image style={styles.image} source={{ uri: image }} />
+        ) : (
+          <Text>Pick an Image</Text>
+        )}
+      </View>
+      <OutlinedButton icon="camera" onPress={pickImageHandler}>
+        Pick Image
+      </OutlinedButton>
     </View>
   );
 };
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "200%",
+  },
+});
